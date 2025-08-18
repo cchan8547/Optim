b2cb9684e257b245fabe1362b8794ae6c9355ea8
cchan8547 <cchan8547@gmail.com>
2025-08-18 14:20:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 
-page_7251 = st.Page("7251.py", title="7251")
-page_8021 = st.Page("8021.py", title="8021")
-page_812 = st.Page("812.py", title="812")
+page_7251 = st.Page("7251.py", title="Model 1")
+page_8021 = st.Page("8021.py", title="Model 2")
+page_812 = st.Page("812.py", title="Model 3")
 # 建立導航
 pg = st.navigation([page_7251, page_8021, page_812])
 
